158eb6a9c6cbe030c730e256a254df958a3fe122
Peter <pmuniz03@gmail.com>
2017-06-20 15:41:46 -0500

    Added

--- port/script.js
+++ port/script.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
   $("#burger").click(function(){
     $("#burgerMenu").slideToggle("slow", function(){
       $("#burgerMenu").css("visibility","visible");
+    });
   });
-});
+
+  $(".typing").typeIt({
+      strings:['websites.','web applications.','landing pages.'],
+      speed: 90,
+      breakLines: false,
+      loop: true,
+      deleteDelay: 2000,
+      loopDelay: 2000
+  })
 });
